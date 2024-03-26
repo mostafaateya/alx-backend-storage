524d753593985d662b831d2334a4378e51e407e1
mostafaateya <mostafaateya@gmail.com>
2024-03-26 13:54:40 -0700

    Update

--- 0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- 0. We are all unique!
--- creates a table users
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT, 
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-);
